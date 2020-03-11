06065c8d87b369ecc84f911e0c51967225b72415
yuanxiaowei <yuanxiaowei@chuangjiangx.com>
2020-03-11 22:10:36 +0800

    build

---
+++ build/precache-manifest.26bce7baa6554b9b8950748a2e28af42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f694bd6d888c7e86523579223998a84d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbfaff0ddb575cc6d73c",
+    "url": "/static/css/2.4a92bbca.chunk.css"
+  },
+  {
+    "revision": "dce92c87d6f01da1ad80",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cbfaff0ddb575cc6d73c",
+    "url": "/static/js/2.4b5ca2e3.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.4b5ca2e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dce92c87d6f01da1ad80",
+    "url": "/static/js/main.919df64e.chunk.js"
+  },
+  {
+    "revision": "be4fae2964695223cfa7",
+    "url": "/static/js/runtime-main.6086f715.js"
+  }
+]);
